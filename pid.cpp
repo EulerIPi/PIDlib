6945157c779643ac08d2523710b8c64159f45553
EulerIPi <samuelrbartz@gmail.com>
2026-04-27 14:27:02 -0300

    Update pid.cpp

--- pid.cpp
+++ pid.cpp
@@ -1,17 +1,33 @@
-pid(float kp, float ki, float kd){
+PID(float kp, float ki, float kd, float constReset = 5000.0f, float MAXall = 9999.0f, float MAXp = 9999.0f, float MAXi = 9999.0f, float MAXd = 9999.0f){
     this->kd = kd;
     this->ki = ki;
     this->kp = kp;
+    this->constReset = constReset;
+    this-> MAXall = MAXall;
+    this-> MAXp = MAXp;
+    this-> MAXi = MAXi;
+    this-> MAXd = MAXd;
     for(int i = 0; i< BUFFER_SIZE; i++) buffer[i] = 0.0f;
 }
 
-float pid::get(float set, float ret){
+float PID::constrainPID(float val, float lim){
+    return (val > lim || -val < -lim)? lim : val;
+}
+
+float PID::get(float set, float ret){
     error = set - ret;
     time = millis();
     deltaT = time - timeAnt;
     timeAnt = time;
+
+    if(error > constReset){
+        for(int i = 0; i< BUFFER_SIZE; i++) buffer[i] = 0.0f;
+        start = true;
+        errorAnt = 0.0f;
+    }
+
     //Proportional:
-    prop = kp*error;
+    prop = constrainPID(kp*error, MAXp);
     
     //Integrative:
 
@@ -23,19 +39,18 @@ float pid::get(float set, float ret){
         integ -= buffer[count];
     }
     buffer[count] = error*deltaT;
-    integ *= ki;
-
+    integ = constrainPID(integ*ki, MAXi);
     //Derivative:
-    if(inicio){
+    if(start){
         der = 0.0f;
-        inicio = false;
+        start = false;
     }else if(deltaT == 0){
-        der = FLT_MAX;
+        der = MAXd;
     }else{
-        der kd*1000*(error - errorAnt)/deltaT;
+        der = constrainPID(kd*1000*(error - errorAnt)/deltaT, MAXd);
     }
     
     errorAnt = error;
     count = (count+1)%BUFFER_SIZE;
-    return prop + integ + der;
+    return constrainPID(prop + integ + der, MAXall);
 }
