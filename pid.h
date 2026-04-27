32eb22b42b64875ae2706dc4faaecb25dd7b6414
EulerIPi <samuelrbartz@gmail.com>
2026-04-27 14:35:25 -0300

    Update pid.h

--- pid.h
+++ pid.h
@@ -9,20 +9,20 @@ class PID{
     float buffer[BUFFER_SIZE];
     float prop, integ = 0, der;
     float kp, ki, kd;
-    float MAX[3];
+    float MAXp, MAXi, MAXd, MAXall;
     unsigned long timeAnt = 0, time;
     bool start = true;
     float errorAnt = 0, error, deltaT = 0;
     float auxInteg;
     float out;
-    float setAnt = 0.0f, constReset = 9999.0;
+    float setAnt = 0.0f, constReset;
     typedef enum{
       P,
       I,
       D,
       ALL
     }PID_TYPE;
-    float PID::limitter(float val, float lim)
+    float limitter(float val, float lim)
   public:
     PID(float kp, float ki, float kd, float constReset = 5000.0f, float MAXall = 9999.0f, float MAXp = 9999.0f, float MAXi = 9999.0f, float MAXd = 9999.0f);
     float get(float set, float ret);
